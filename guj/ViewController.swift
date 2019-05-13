026369550a63e0f3ea7351ae8ff9751f78522da3
Malik McIntosh <malik.mcintosh@brooklynsteamcenter.org>
2019-05-13 13:04:36 -0400

    asd

--- guj/ViewController.swift
+++ guj/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Getchanged: UILabel!
+    @IBAction func textual(_ sender: Any) {
+        
+        Getchanged.textColor = UIColor.red
+        Getchanged.text = "My"
+    }
+    @IBAction func Danger(_ sender: Any) {
+        Getchanged.text = "Will"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
